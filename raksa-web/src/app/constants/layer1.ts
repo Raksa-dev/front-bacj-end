c89e0b2306bcb970734879a0bfda4f2702a2db44
moses12345 <shaikmoses1997@gmail.com>
2024-06-30 15:14:02 +0530

    layer 1 constants

---
+++ raksa-web/src/app/constants/layer1.ts
@@ -0,0 +1,280 @@
+export const layer1_male_ascendants = {
+  'Aries Ascendant': {
+    description:
+      "Alex is a powerhouse of energy, always the first to take on a new challenge. Whether it's a new project at work or an outdoor adventure, Alex dives in headfirst with boldness and enthusiasm. His competitive spirit drives him to be a leader in every aspect of his life. While he can be impulsive, his independent nature ensures he always finds his own path. Alex is assertive, often taking charge in group settings, and his friends admire his fearless attitude.",
+    Hollywood: [
+      "John McClane from 'Die Hard'",
+      "Indiana Jones from 'Indiana Jones'",
+    ],
+    Bollywood: [
+      "Ranveer Singh's character in 'Gully Boy'",
+      "Salman Khan's character in 'Sultan'",
+    ],
+  },
+  'Taurus Ascendant': {
+    description:
+      "Sam is the rock everyone leans on. He has a calm, practical approach to life and finds joy in simple pleasures. Whether he's tending to his garden, enjoying a gourmet meal, or relaxing in a cozy setting, Sam's love for comfort and nature is evident. His patience and reliability make him a steadfast friend and colleague. Though sometimes perceived as stubborn, Sam's determination is unmatched, and he values stability and sensuality in all aspects of his life.",
+    Hollywood: [
+      "Hagrid from 'Harry Potter'",
+      "Samwise Gamgee from 'The Lord of the Rings'",
+    ],
+    Bollywood: [
+      "Rishi Kapoor's character in 'Kapoor & Sons'",
+      "Irrfan Khan's character in 'The Lunchbox'",
+    ],
+  },
+  'Gemini Ascendant': {
+    description:
+      "Jamie is a social butterfly, always buzzing from one conversation to the next. His curiosity leads him to explore a wide array of interests, making him incredibly versatile and knowledgeable. Jamie's quick wit and charm make him the life of any gathering. He thrives on communication, whether through writing, speaking, or social media. His restless nature means he's always on the move, seeking new experiences and connections.",
+    Hollywood: ["Tony Stark from 'Iron Man'", "Peter Parker from 'Spider-Man'"],
+    Bollywood: [
+      "Ranbir Kapoor's character in 'Yeh Jawaani Hai Deewani'",
+      "Ayushmann Khurrana's character in 'Vicky Donor'",
+    ],
+  },
+  'Cancer Ascendant': {
+    description:
+      'Casey is the caregiver of the group, deeply empathetic and intuitive. Family and home are sacred to Casey, and he has a natural talent for creating cozy, welcoming spaces. His sensitivity allows him to connect with others on an emotional level, though this can also make him prone to mood swings. Loyal to the core, Casey is someone you can always count on for support.',
+    Hollywood: [
+      "Samwise Gamgee from 'The Lord of the Rings'",
+      "Charlie Kelmeckis from 'The Perks of Being a Wallflower'",
+    ],
+    Bollywood: [
+      "Shah Rukh Khan's character in 'My Name is Khan'",
+      "Ranbir Kapoor's character in 'Barfi!'",
+    ],
+  },
+  'Leo Ascendant': {
+    description:
+      "Leo is a natural performer, radiating confidence and charisma. He loves being in the spotlight and is often the center of attention at social events. Generous and warm-hearted, Leo enjoys making others feel special and valued. His dramatic flair and leadership skills often see him taking charge of situations, and his pride in his achievements is evident. Leo's larger-than-life personality and sunny disposition make him a joy to be around.",
+    Hollywood: ["James Bond from 'James Bond'", "Tony Stark from 'Iron Man'"],
+    Bollywood: [
+      "Ranveer Singh's character in 'Bajirao Mastani'",
+      "Hrithik Roshan's character in 'War'",
+    ],
+  },
+  'Virgo Ascendant': {
+    description:
+      'Aiden is the epitome of precision and detail. He approaches life with an analytical mind, always seeking to improve and perfect. Modest and unassuming, Aiden is incredibly helpful and reliable. His critical eye and practical nature make him a master organizer. While he can be a bit of a perfectionist, his dedication to efficiency and his hardworking spirit are commendable.',
+    Hollywood: [
+      "Sherlock Holmes from 'Sherlock Holmes'",
+      "Spock from 'Star Trek'",
+    ],
+    Bollywood: [
+      "Ranbir Kapoor's character in 'Rocket Singh: Salesman of the Year'",
+      "Aamir Khan's character in '3 Idiots'",
+    ],
+  },
+  'Libra Ascendant': {
+    description:
+      'Ben is the diplomat, always striving for balance and harmony. He has a natural charm and grace that draws people to him. Ben values fairness and often finds himself mediating conflicts, ensuring everyone feels heard. His sociable nature makes him a beloved friend and companion. Artistic and aesthetically inclined, Ben appreciates beauty in all forms and seeks to create a peaceful environment around him.',
+    Hollywood: [
+      "Gandalf from 'The Lord of the Rings'",
+      "Atticus Finch from 'To Kill a Mockingbird'",
+    ],
+    Bollywood: [
+      "Shah Rukh Khan's character in 'Dilwale Dulhania Le Jayenge'",
+      "Amitabh Bachchan's character in 'Piku'",
+    ],
+  },
+  'Scorpio Ascendant': {
+    description:
+      "Jill is an enigma, possessing an intensity and depth that intrigues those around him. Passionate and determined, he approaches life with a fierce drive. Jill is incredibly resilient, able to overcome obstacles through sheer willpower. His secretive nature means he often keeps his deepest feelings to himself, but his magnetic presence is undeniable. Jill's emotional depth and mystery make him a powerful and captivating individual.",
+    Hollywood: ["Bruce Wayne from 'Batman'", "James Bond from 'James Bond'"],
+    Bollywood: [
+      "Ranveer Singh's character in 'Padmaavat'",
+      "Shah Rukh Khan's character in 'Don'",
+    ],
+  },
+  'Sagitarius Ascendant': {
+    description:
+      'Max is the explorer, always on the hunt for new adventures. His optimistic outlook and love for freedom drive him to seek out new experiences and knowledge. Honest to a fault, Max values truth and integrity. He thrives in open, dynamic environments and loves to travel. His independent spirit and philosophical nature lead him to question the world around him, always looking for the bigger picture.',
+    Hollywood: [
+      "Indiana Jones from 'Indiana Jones'",
+      "Han Solo from 'Star Wars'",
+    ],
+    Bollywood: [
+      "Ranbir Kapoor's character in 'Yeh Jawaani Hai Deewani'",
+      "Aamir Khan's character in 'Dil Chahta Hai'",
+    ],
+  },
+  'Capricorn Ascendant': {
+    description:
+      'Chris is the embodiment of ambition and discipline. He has a strong sense of duty and a practical approach to achieving his goals. Reserved and responsible, Chris often takes on leadership roles, guiding others with his methodical planning. He values tradition and structure, and his perseverance ensures he always reaches his objectives. Chris’s goal-oriented mindset and reliability make him a respected figure in any setting.',
+    Hollywood: [
+      "Captain America from 'Captain America'",
+      "Maximus from 'Gladiator'",
+    ],
+    Bollywood: [
+      "Amitabh Bachchan's character in 'Agneepath'",
+      "Hrithik Roshan's character in 'Super 30'",
+    ],
+  },
+  'Aquarius Ascendant': {
+    description:
+      'Taylor is the innovator, constantly thinking outside the box. His independent streak and intellectual prowess make him a forward-thinker. Taylor values freedom and often engages in humanitarian efforts, aiming to improve the world around him. His eccentric nature sets him apart, and he enjoys exploring unconventional ideas. Detached when necessary, Taylor maintains a broad perspective, always looking towards the future.',
+    Hollywood: ["Doc Brown from 'Back to the Future'", "Neo from 'The Matrix'"],
+    Bollywood: [
+      "Ranbir Kapoor's character in 'Tamasha'",
+      "Farhan Akhtar's character in 'Zindagi Na Milegi Dobara'",
+    ],
+  },
+  'Pisces Ascendant': {
+    description:
+      "Emma is a dreamer, with a compassionate and imaginative soul. He has a natural sensitivity that allows him to connect deeply with others. Often lost in thought, Emma's creativity knows no bounds. He can be escapist, preferring to dwell in his rich inner world. Adaptable and intuitive, Emma is deeply empathetic and often finds himself drawn to artistic and spiritual pursuits. His dreamy nature and gentle disposition make him a soothing presence.",
+    Hollywood: [
+      "Frodo Baggins from 'The Lord of the Rings'",
+      "Edward Scissorhands from 'Edward Scissorhands'",
+    ],
+    Bollywood: [
+      "Ranbir Kapoor's character in 'Barfi!'",
+      "Shah Rukh Khan's character in 'Swades'",
+    ],
+  },
+};
+
+export const layer1_female_ascendants = {
+  'Aries Ascendant': {
+    description:
+      "Alex is a powerhouse of energy, always the first to take on a new challenge. Whether it's a new project at work or an outdoor adventure, Alex dives in headfirst with boldness and enthusiasm. Her competitive spirit drives her to be a leader in every aspect of her life. While she can be impulsive, her independent nature ensures she always finds her own path. Alex is assertive, often taking charge in group settings, and her friends admire her fearless attitude.",
+    Hollywood: [
+      "Katniss Everdeen from 'The Hunger Games'",
+      "Lara Croft from 'Tomb Raider'",
+    ],
+    Bollywood: [
+      "Geet Dhillon from 'Jab We Met'",
+      "Rani Mehra from 'Mardaani'",
+      "Rey from 'Star Wars'",
+      "Diana Prince from 'Wonder Woman'",
+      "Sarah Connor from 'Terminator'",
+      "Rani from 'Queen'",
+      "Shivani Shivaji Roy from 'Mardaani'",
+      "Anushka Sharma's character in 'Sultan'",
+    ],
+  },
+  'Taurus Ascendant': {
+    description:
+      "Sam is the rock everyone leans on. She has a calm, practical approach to life and finds joy in simple pleasures. Whether she's tending to her garden, enjoying a gourmet meal, or relaxing in a cozy setting, Sam's love for comfort and nature is evident. Her patience and reliability make her a steadfast friend and colleague. Though sometimes perceived as stubborn, Sam's determination is unmatched, and she values stability and sensuality in all aspects of her life.",
+    Hollywood: [
+      "Belle from 'Beauty and the Beast'",
+      "Clarice Starling from 'The Silence of the Lambs'",
+      "Bridget Jones from 'Bridget Jones's Diary'",
+      "Leslie Knope from 'Parks and Recreation'",
+      "Tiana from 'The Princess and the Frog'",
+    ],
+    Bollywood: [
+      "Piku Banerjee from 'Piku'",
+      "Naina Mathur from 'Hichki'",
+      "Shashi from 'English Vinglish'",
+      "Tara from 'Tamasha'",
+    ],
+  },
+  'Gemini Ascendant': {
+    description:
+      "Jamie is a social butterfly, always buzzing from one conversation to the next. Her curiosity leads her to explore a wide array of interests, making her incredibly versatile and knowledgeable. Jamie's quick wit and charm make her the life of any gathering. She thrives on communication, whether through writing, speaking, or social media. Her restless nature means she's always on the move, seeking new experiences and connections.",
+    Hollywood: [
+      "Holly Golightly from 'Breakfast at Tiffany's'",
+      "Elle Woods from 'Legally Blonde'",
+    ],
+    Bollywood: ["Rani Mehra from 'Queen'", "Veronica Malaney from 'Cocktail'"],
+  },
+  'Cancer Ascendant': {
+    description:
+      'Casey is the caregiver of the group, deeply empathetic and intuitive. Family and home are sacred to Casey, and she has a natural talent for creating cozy, welcoming spaces. Her sensitivity allows her to connect with others on an emotional level, though this can also make her prone to mood swings. Loyal to the core, Casey is someone you can always count on for support.',
+    Hollywood: [
+      "Molly Weasley from 'Harry Potter'",
+      "Evelyn Abbott from 'A Quiet Place'",
+    ],
+    Bollywood: [
+      "Naina Talwar from 'Yeh Jawaani Hai Deewani'",
+      "Simran from 'Dilwale Dulhania Le Jayenge'",
+    ],
+  },
+  'Leo Ascendant': {
+    description:
+      "Leo is a natural performer, radiating confidence and charisma. She loves being in the spotlight and is often the center of attention at social events. Generous and warm-hearted, Leo enjoys making others feel special and valued. Her dramatic flair and leadership skills often see her taking charge of situations, and her pride in her achievements is evident. Leo's larger-than-life personality and sunny disposition make her a joy to be around.",
+    Hollywood: [
+      "Mia Dolan from 'La La Land'",
+      "Regina George from 'Mean Girls'",
+    ],
+    Bollywood: [
+      "Geet Dhillon from 'Jab We Met'",
+      "Pooja (Poo) from 'Kabhi Khushi Kabhie Gham'",
+    ],
+  },
+  'Virgo Ascendant': {
+    description:
+      'Ava is the epitome of precision and detail. She approaches life with an analytical mind, always seeking to improve and perfect. Modest and unassuming, Ava is incredibly helpful and reliable. Her critical eye and practical nature make her a master organizer. While she can be a bit of a perfectionist, her dedication to efficiency and her hardworking spirit are commendable.',
+    Hollywood: [
+      "Hermione Granger from 'Harry Potter'",
+      "Clarice Starling from 'The Silence of the Lambs'",
+    ],
+    Bollywood: [
+      "Ananya Swaminathan from '2 States'",
+      "Vidya Bagchi from 'Kahaani'",
+    ],
+  },
+  'Libra Ascendant': {
+    description:
+      'Bella is the diplomat, always striving for balance and harmony. She has a natural charm and grace that draws people to her. Bella values fairness and often finds herself mediating conflicts, ensuring everyone feels heard. Her sociable nature makes her a beloved friend and companion. Artistic and aesthetically inclined, Bella appreciates beauty in all forms and seeks to create a peaceful environment around her.',
+    Hollywood: [
+      "Elle Woods from 'Legally Blonde'",
+      "Belle from 'Beauty and the Beast'",
+    ],
+    Bollywood: [
+      "Naina Mathur from 'Hichki'",
+      "Simran from 'Dilwale Dulhania Le Jayenge'",
+    ],
+  },
+  'Scorpio Ascendant': {
+    description:
+      "Jill is an enigma, possessing an intensity and depth that intrigues those around her. Passionate and determined, she approaches life with a fierce drive. Jill is incredibly resilient, able to overcome obstacles through sheer willpower. Her secretive nature means she often keeps her deepest feelings to herself, but her magnetic presence is undeniable. Jill's emotional depth and mystery make her a powerful and captivating individual.",
+    Hollywood: [
+      "Lisbeth Salander from 'The Girl with the Dragon Tattoo'",
+      "Daenerys Targaryen from 'Game of Thrones'",
+    ],
+    Bollywood: ["Veronica Malaney from 'Cocktail'", "Meera from 'NH10'"],
+  },
+  'Sagitarius Ascendant': {
+    description:
+      'Max is the explorer, always on the hunt for new adventures. Her optimistic outlook and love for freedom drive her to seek out new experiences and knowledge. Honest to a fault, Max values truth and integrity. She thrives in open, dynamic environments and loves to travel. Her independent spirit and philosophical nature lead her to question the world around her, always looking for the bigger picture.',
+    Hollywood: [
+      "Lara Croft from 'Tomb Raider'",
+      "Wonder Woman from 'Wonder Woman'",
+    ],
+    Bollywood: [
+      "Kangana Ranaut's character in 'Queen'",
+      "Alia Bhatt's character in 'Dear Zindagi'",
+    ],
+  },
+  'Capricorn Ascendant': {
+    description:
+      'Chris is the embodiment of ambition and discipline. She has a strong sense of duty and a practical approach to achieving her goals. Reserved and responsible, Chris often takes on leadership roles, guiding others with her methodical planning. She values tradition and structure, and her perseverance ensures she always reaches her objectives. Chris’s goal-oriented mindset and reliability make her a respected figure in any setting.',
+    Hollywood: [
+      "Miranda Priestly from 'The Devil Wears Prada'",
+      "Sarah Connor from 'Terminator'",
+    ],
+    Bollywood: [
+      "Priyanka Chopra's character in 'Mary Kom'",
+      "Vidya Balan's character in 'Kahaani'",
+    ],
+  },
+  'Aquarius Ascendant': {
+    description:
+      'Taylor is the innovator, constantly thinking outside the box. Her independent streak and intellectual prowess make her a forward-thinker. Taylor values freedom and often engages in humanitarian efforts, aiming to improve the world around her. Her eccentric nature sets her apart, and she enjoys exploring unconventional ideas. Detached when necessary, Taylor maintains a broad perspective, always looking towards the future.',
+    Hollywood: ["Luna Lovegood from 'Harry Potter'", "Amélie from 'Amélie'"],
+    Bollywood: [
+      "Deepika Padukone's character in 'Tamasha'",
+      "Kangana Ranaut's character in 'Panga'",
+    ],
+  },
+  'Pisces Ascendant': {
+    description:
+      "Emma is a dreamer, with a compassionate and imaginative soul. She has a natural sensitivity that allows her to connect deeply with others. Often lost in thought, Emma's creativity knows no bounds. She can be escapist, preferring to dwell in her rich inner world. Adaptable and intuitive, Emma is deeply empathetic and often finds herself drawn to artistic and spiritual pursuits. Her dreamy nature and gentle disposition make her a soothing presence.",
+    Hollywood: ["Amélie Poulain from 'Amélie'", "Rapunzel from 'Tangled'"],
+    Bollywood: [
+      "Ayesha Mehra from 'Dil Dhadakne Do'",
+      "Kaira from 'Dear Zindagi'",
+    ],
+  },
+};
