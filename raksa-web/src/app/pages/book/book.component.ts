b423cda7043dcbb0312b18768305e512fe39384c
moses12345 <shaikmoses1997@gmail.com>
2024-07-06 11:12:52 +0530

    added gooogle analytics tag

--- raksa-web/src/app/pages/book/book.component.ts
+++ raksa-web/src/app/pages/book/book.component.ts
@@ -31,6 +31,7 @@ import { AuthService, UserService } from 'src/app/core/services';
 import { Observable, Subject, of, throwError } from 'rxjs';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { WalletComponent } from 'src/app/shared/wallet/wallet.component';
+import { QUESTIONS } from 'src/app/constants/questions';
 
 @Component({
   selector: 'app-book',
@@ -40,76 +41,7 @@ import { WalletComponent } from 'src/app/shared/wallet/wallet.component';
 export class BookComponent implements OnInit {
   selectedCategory: string;
   checkboxForm: FormGroup;
-  public categoricalQuestion = {
-    self_awareness: [
-      'Can you provide insights about my personality based on my birth chart ?',
-      'What are the strengths and weaknesses indicated in my birth chart?',
-      'Generate insights about my general demeanor and approach to life based on my ascendant sign',
-      'How do the planets housed in my ascendant house impact my personality?',
-      'How does my Sun sign contribute to my personality traits?',
-      'What insights can you provide about my personality from the planetary positions in different " \
-      "houses in my vedic horoscope? ',
-      'How do I typically appear to others upon first impression  ?',
-      'What are my primary motivations and desires in life  ?',
-      'How do I typically respond to new situations and challenges ?',
-      'What are my primary motivations and desires in life according to my astrological information ?',
-      'How do I typically respond to new situations and challenges according to my astrological information ?',
-      'What are my natural strengths and talents according to my astrological information ?',
-      'What are the negatives in my horoscope ?',
-      'How do I tend to approach relationships and social interactions according to my astrological information ?',
-      'How do I express myself creatively and artistically according to my astrological information ?',
-      'What are my natural inclinations and preferences in terms of lifestyle and environment according to my astrological information ?',
-      'What are my tendencies in terms of self-care and well-being according to my astrological information ?',
-      'What steps can I take to enhance my overall well-being and happiness according to my astrological information ?',
-    ],
-    family: [
-      'What does my birth chart suggest about my family dynamics?',
-      'How do the planetary positions in my birth chart influence my relationships with family " \
-      "members?',
-      'Are there any specific planetary placements that indicate challenges or conflicts within my " \
-      "family?',
-      'Are there any karmic or past-life connections indicated in my birth chart that influence my " \
-      "family dynamics? ',
-      'What role do the Moon and Venus placements play in shaping my emotional connections and bonds " \
-      "with family members?',
-      'Are there any astrological remedies or practices I can incorporate to foster harmony and " \
-      "unity within my family?',
-    ],
-    wealth: [
-      'Can astrology provide insights into my spending habits and tendencies based on my astrological profile',
-      'What career paths or investment opportunities align with my astrological profile',
-      'How can I mitigate financial risks and maximize opportunities based on my astrological profile',
-      'What steps can I take to manifest abundance and financial success based on my astrological profile',
-      'How can I align my financial goals with my life purpose and astrological chart based on my astrological profile',
-      'What is my natural inclination towards handling money and finances based on my astrological profile',
-      'What areas of my life should I focus on to improve my overall financial well-being based on " \
-      "my astrological profile',
-      'Are there any planetary placements or aspects that indicate a need for caution in financial matters based on my astrological profile',
-      'What role does karma or past-life influences play in shaping my current financial situation based on my astrological profile',
-      'Can astrology help me identify the most auspicious timing for major financial decisions or investments?',
-    ],
-    health: [
-      'Can you predict any health issues or concerns in the near future based on my astrological profile?',
-      'What sort of health issues will I have according to my astrological profile? ',
-      'How can I maintain good physical health based on my birth chart?',
-      'Give me 5 strengths and weaknesses related to health based on my astrological profile?',
-      'Will I experience emotional stability or fluctuations in mood based on my astrological profile?',
-      'Can you suggest coping strategies or therapies for emotional healing based on my astrological profile?',
-    ],
-    numerology: [
-      'What insights can numerology provide about my personality traits, strengths, and weaknesses?',
-      'How does my life path number influence the major events and opportunities in my life?',
-      'Can numerology help me understand my relationships with others, including family, friends, and romantic partners?',
-      'Are there any significant numbers or patterns in my numerology chart that indicate areas of focus or potential challenges?',
-      'How can I use numerology to make informed decisions about my career, finances, and personal growth?',
-    ],
-    travel: [
-      'Can you predict any travel opportunities or relocations for me based on my astrological profile?',
-      'Can you suggest destinations that align with my astrological profile based on my astrological profile?',
-      'How can I ensure safe and enjoyable travels based on my birth chart?',
-    ],
-    business: [],
-  };
+  public categoricalQuestion = QUESTIONS;
 
   layer2Question = [
     {
